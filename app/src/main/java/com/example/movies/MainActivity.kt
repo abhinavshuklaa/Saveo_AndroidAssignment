aa80b142527718149ba0a9618d3fd2e5c97d788b
Abhinav Shukla <abhinav2014shukla@gmail.com>
2021-07-25 23:19:11 +0530

    Completed the layouts and Click Handling. Onto adding comments to functions

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.movies
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.*
@@ -17,19 +18,26 @@ class MainActivity : AppCompatActivity(), RecyclerViewItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /**
+         * setting the custom action bar as per the design
+         */
+        getSupportActionBar()?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar()?.setCustomView(R.layout.abs_main_activity)
         recyclerViewVertical = findViewById(R.id.recyclerViewVerticalGrids)
         recyclerView = findViewById(R.id.recyclerViewHorizontal)
         initRecyclerView()
         initViewModel()
     }
 
+    /**
+     * Setting the recycler view using scope function
+     */
     private fun initRecyclerView() {
 
         recyclerViewVertical.apply {
             layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
 
             horizontalViewAdapter = HorizontalViewAdapter(this@MainActivity)
-//            adapter = horizontalViewAdapter
 
         }
 
@@ -45,6 +53,9 @@ class MainActivity : AppCompatActivity(), RecyclerViewItemClickListener {
         }
     }
 
+    /**
+     * Initialising the ViewModel pattern
+     */
     private fun initViewModel() {
         val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         viewModel.getRecyclerListObserver()?.observe(this, {
@@ -62,24 +73,23 @@ class MainActivity : AppCompatActivity(), RecyclerViewItemClickListener {
         })
     }
 
+    /**
+     * Overridden method to handle the clicks and transferring data
+     */
     override fun onItemClicked(position: Int, results: ResponseModelItem) {
-        val intent = Intent(this,MovieDetailsActivity::class.java)
-        intent.putExtra("movieName",results.name.toString())
-        intent.putExtra("premiered",results.premiered.toString())
-        intent.putExtra("summary",results.summary.toString())
-        val arrayList:ArrayList<String> = ArrayList()
-        for( i in results.genres!!.indices){
-            arrayList.add(results.genres.get(i))
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        intent.putExtra("movieName", results.name.toString())
+        intent.putExtra("premiered", results.premiered.toString())
+        intent.putExtra("summary", results.summary.toString())
+        val arrayList: ArrayList<String> = ArrayList()
+        for (i in results.genres!!.indices) {
+            arrayList.add(results.genres[i])
         }
-        intent.putExtra("genre",arrayList)
-        intent.putExtra("rating",results.rating.toString())
-        intent.putExtra("image",results.image!!.original).toString()
+        intent.putExtra("genre", arrayList)
+        intent.putExtra("rating", results.rating.toString())
+        intent.putExtra("duration", results.runtime).toString()
+        intent.putExtra("image", results.image!!.original).toString()
         startActivity(intent)
-
-
-
-
-
 
 
     }
--- app/src/main/java/com/example/movies/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/movies/MovieDetailsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.movies
 
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_movie_details.*
@@ -8,6 +10,8 @@ import kotlinx.android.synthetic.main.activity_movie_details.*
 class MovieDetailsActivity : AppCompatActivity() {
     private lateinit var name: String
     private lateinit var imageUrl: String
+
+    private lateinit var duration: String
     private lateinit var premeire: String
     private lateinit var summary: String
     private lateinit var rating: String
@@ -17,6 +21,8 @@ class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+        getSupportActionBar()?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar()?.setCustomView(R.layout.abs_layout);
         getDataFromIntent()
         setDataFromIntent()
     }
@@ -25,9 +31,19 @@ class MovieDetailsActivity : AppCompatActivity() {
         tvSummary.text = summary
         tvMovieName.text = name
         Glide.with(ivMovieImage).load(imageUrl).centerCrop().into(ivMovieImage)
-        tvRatings.text = rating
-        tvPremiereDate.text = premeire
-        tvGenre.text = list.get(0)
+        tvRatings.text = "⭐⭐⭐⭐${rating}"
+        tvPremiereDate.text = "R|${duration}|${premeire}"
+        if (list.size != 0) {
+            tvGenre.text = list.get(0)
+            tvGenre_1.visibility=View.INVISIBLE
+            tvGenre_2.visibility=View.INVISIBLE
+
+        } else if (list.size == 2)
+            tvGenre_1.text = list.get(1)
+        else if (list.size == 3) tvGenre_2.text = list.get(2)
+        else {
+            tvGenre.text = "No Data"
+        }
 
     }
 
@@ -36,6 +52,7 @@ class MovieDetailsActivity : AppCompatActivity() {
         premeire = intent.getStringExtra("premiered").toString()
         summary = intent.getStringExtra("summary").toString()
         rating = intent.getStringExtra("rating").toString()
+        duration = intent.getStringExtra("duration").toString()
         imageUrl = intent.getStringExtra("image").toString()
         list = intent.getStringArrayListExtra("genre")!!
     }
